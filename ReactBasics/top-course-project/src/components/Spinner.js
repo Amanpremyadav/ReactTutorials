d4e180029461700175fd4f119d22ee01b2a814fb
Amanyadav <amanyadav9675maa@gmail.com>
2023-12-04 10:33:32 +0530

    Top-course-project completed

---
+++ ReactBasics/top-course-project/src/components/Spinner.js
@@ -0,0 +1,10 @@
+ const Spinner = () =>{
+    return (
+      <div>
+         <div className="spinner flex flex-col items-center space-y-2"></div>
+         <p className="text-bgDark text-lg font-semibold">Loading...</p>
+      </div>
+    );
+ };
+
+ export default Spinner;
